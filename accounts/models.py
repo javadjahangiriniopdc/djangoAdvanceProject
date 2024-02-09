896fc6346d67fdb2084ec10c08413d4194ae6e9b
jahangiri jahangiri <javad.jahangiri.niopdc@gmail.com>
2024-02-09 21:44:32 +0330

    add UserManager ,Set for Default User Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 
+from accounts.managers import UserManager
+
 
 # Create your models here.
 class User(AbstractBaseUser):
@@ -9,6 +11,8 @@ class User(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     fullname = models.CharField(max_length=50)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS = ['email', 'fullname']
