86d37594b3f6ea5c9f38e4ef84747d203e534bad
jahangiri jahangiri <javad.jahangiri.niopdc@gmail.com>
2024-02-16 20:07:08 +0330

    add forms UserCreationForm and UserChangeForm

---
+++ accounts/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from .models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone_number', 'fullname')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        cd = self.cleaned_data
+        password1 = cd["password1"]
+        password2 = cd["password2"]
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password2'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(help_text='you can change password using <a href= "../password/" > this form '
+                                                   '</a>')
+
+    class Meta:
+        model = User
+        fields = ('email', 'phone_number', 'fullname', 'password', 'last_login')
