18b3e82cfd56e49c0566c367fb94c307c5277f3e
jahangiri jahangiri <javad.jahangiri.niopdc@gmail.com>
2024-02-25 17:10:06 +0330

    add user_register link to navbar

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 from accounts import views
 
+app_name = 'accounts'
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='user_register')
 ]
