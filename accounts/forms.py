4e2cd4f77b7293bf057f80f67a6292f534d6f465
jahangiri jahangiri <javad.jahangiri.niopdc@gmail.com>
2024-02-25 17:00:33 +0330

    add UserRegistrationForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -38,3 +38,10 @@ class UserChangeForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'phone_number', 'fullname', 'password', 'last_login')
+
+
+class UserRegistrationForm(forms.Form):
+    email = forms.EmailField()
+    full_name = forms.CharField(label='full name')
+    phone = forms.CharField(max_length=11)
+    password = forms.CharField(widget=forms.PasswordInput)
