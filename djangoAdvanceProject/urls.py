c74732535edc348ed7d12236cff76ec6faaf2840
jahangiri jahangiri <javad.jahangiri.niopdc@gmail.com>
2024-03-15 22:16:07 +0330

    config access media file in debug mode

--- djangoAdvanceProject/urls.py
+++ djangoAdvanceProject/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+# *** in production mode clear static config from this file ***
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls', namespace='home')),
-    path('accounts/', include('accounts.urls', namespace='accounts'))
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('home.urls', namespace='home')),
+                  path('accounts/', include('accounts.urls', namespace='accounts'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# *** in production mode clear static config from this file ***
